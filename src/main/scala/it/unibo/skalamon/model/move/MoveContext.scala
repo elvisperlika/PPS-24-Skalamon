25f71b334905002f4de0a2cbcf0bedc225c8ad3f
Giorgio Garofalo <giogar2705@gmail.com>
2025-06-06 14:29:49 +0200

    chore: add missing MoveContext documentation

--- src/main/scala/it/unibo/skalamon/model/move/MoveContext.scala
+++ src/main/scala/it/unibo/skalamon/model/move/MoveContext.scala
@@ -2,16 +2,31 @@ package it.unibo.skalamon.model.move
 
 import it.unibo.skalamon.model.pokemon.MutablePokemon
 
-/**
- *
- */
+/** Represents the context of a move that can be executed in a battle.
+  *
+  * @param move
+  *   The move this context is associated with.
+  * @param target
+  *   The target Pokémon of the move.
+  * @param source
+  *   The source Pokémon that is executing the move.
+  * @param hits
+  *   Ordered hits that will occur during the execution of the move, each with
+  *   its own power.
+  */
 case class MoveContext(
-                        move: MutableMove,
-                        target: MutablePokemon,
-                        source: MutablePokemon,
-                        var hits: List[MoveContext.Hit] = List.empty,
-                      )
+    move: MutableMove,
+    target: MutablePokemon,
+    source: MutablePokemon,
+    var hits: List[MoveContext.Hit] = List.empty
+)
 
 object MoveContext {
+
+  /** A single hit caused by a move by [[MoveContext]]'s source on [[MoveContext]]'s target.
+    *
+    * @param power
+    *   The base power of the hit.
+    */
   case class Hit(power: Int)
 }
