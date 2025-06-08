079a32d6655e8e45e1fbd1b2aca8a165446f0fca
noodles <elvis.perlika@studio.unibo.it>
2025-06-08 11:16:32 +0200

    feat: add all types

--- src/main/scala/it/unibo/skalamon/model/types/PokemonType.scala
+++ src/main/scala/it/unibo/skalamon/model/types/PokemonType.scala
@@ -8,54 +8,163 @@ package it.unibo.skalamon.model.types
   * Fire attack will be super effective.
   */
 trait Type:
+  def computeEffectiveness(t: PokemonType): Double
 
-  /** 2x effectiveness
-    * @return
-    */
-  def effectiveAgainst: List[Type]
+/** A Pokémon can have one or two Types.
+  */
+type PokemonType = (Type, Type) | Type
 
-  /** 1/2x effectiveness
-    * @return
-    */
-  def resistedBy: List[Type]
+/** A move can have only one move.
+  */
+type MoveType = Type
 
-  /** 0x effectiveness
-    * @return
-    */
-  def ineffectiveAgainst: List[Type] // per “immune”
+/** Efficacy encapsulate multipliers.
+  */
+enum Efficacy:
+  case Effective
+  case Resisted
+  case Ineffective
 
-case object Electric extends Type {
-  override def effectiveAgainst: List[Type] = List(Water, Flying)
-  override def resistedBy: List[Type] = List(Grass, Electric, Dragon)
-  override def ineffectiveAgainst: List[Type] = List(Ground)
+  def value: Double = this match
+    case Effective   => 2.0
+    case Resisted    => 0.5
+    case Ineffective => 0.0
+
+abstract class TypeImpl extends Type {
+  val effectiveAgainst: List[Type]
+  val resistedBy: List[Type]
+  val ineffectiveAgainst: List[Type]
+  private def isEffectiveAgainst(t: Type): Boolean =
+    effectiveAgainst.contains(t)
+  private def isResistedBy(t: Type): Boolean = resistedBy.contains(t)
+  private def isIneffectiveAgainst(t: Type): Boolean =
+    ineffectiveAgainst.contains(t)
+
+  override def computeEffectiveness(t: PokemonType): Double =
+    t match {
+      case t: Type              => computeSingleEffectiveness(t)
+      case (t1: Type, t2: Type) =>
+        computeSingleEffectiveness(t1) + computeSingleEffectiveness(t2)
+    }
+
+  private def computeSingleEffectiveness(t: Type): Double =
+    import Efficacy.*
+    if isEffectiveAgainst(t) then Effective.value
+    else if isResistedBy(t) then Resisted.value
+    else if isIneffectiveAgainst(t) then Ineffective.value
+    else 1
 }
 
-case object Water extends Type {
-  override def effectiveAgainst: List[Type] = ???
-  override def resistedBy: List[Type] = ???
-  override def ineffectiveAgainst: List[Type] = ???
+case object Normal extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = Nil
+  override val resistedBy: List[MoveType] = List(Rock, Steel)
+  override val ineffectiveAgainst: List[MoveType] = List(Ghost)
 }
 
-case object Flying extends Type {
-  override def effectiveAgainst: List[Type] = ???
-  override def resistedBy: List[Type] = ???
-  override def ineffectiveAgainst: List[Type] = ???
+case object Fire extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Grass, Ice, Bug, Steel)
+  override val resistedBy: List[MoveType] = List(Fire, Water, Rock, Dragon)
+  override val ineffectiveAgainst: List[MoveType] = List()
 }
 
-case object Ground extends Type {
-  override def effectiveAgainst: List[Type] = ???
-  override def resistedBy: List[Type] = ???
-  override def ineffectiveAgainst: List[Type] = ???
+case object Water extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Fire, Ground, Rock)
+  override val resistedBy: List[MoveType] = List(Water, Grass, Dragon)
+  override val ineffectiveAgainst: List[MoveType] = List()
 }
 
-case object Grass extends Type {
-  override def effectiveAgainst: List[Type] = ???
-  override def resistedBy: List[Type] = ???
-  override def ineffectiveAgainst: List[Type] = ???
+case object Grass extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Water, Ground, Rock)
+  override val resistedBy: List[MoveType] =
+    List(Fire, Grass, Poison, Flying, Bug, Dragon, Steel)
+  override val ineffectiveAgainst: List[MoveType] = List()
 }
 
-case object Dragon extends Type {
-  override def effectiveAgainst: List[Type] = ???
-  override def resistedBy: List[Type] = ???
-  override def ineffectiveAgainst: List[Type] = ???
+case object Electric extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Water, Flying)
+  override val resistedBy: List[MoveType] = List(Electric, Grass, Dragon)
+  override val ineffectiveAgainst: List[MoveType] = List(Ground)
+}
+
+case object Ice extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] =
+    List(Grass, Ground, Flying, Dragon)
+  override val resistedBy: List[MoveType] = List(Fire, Water, Ice, Steel)
+  override val ineffectiveAgainst: List[MoveType] = List()
+}
+
+case object Fighting extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] =
+    List(Normal, Ice, Rock, Dark, Steel)
+  override val resistedBy: List[MoveType] =
+    List(Poison, Flying, Psychic, Bug, Fairy)
+  override val ineffectiveAgainst: List[MoveType] = List(Ghost)
+}
+
+case object Poison extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Grass, Fairy)
+  override val resistedBy: List[MoveType] = List(Poison, Ground, Rock, Ghost)
+  override val ineffectiveAgainst: List[MoveType] = List(Steel)
+}
+
+case object Ground extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] =
+    List(Fire, Electric, Poison, Rock, Steel)
+  override val resistedBy: List[MoveType] = List(Grass, Bug)
+  override val ineffectiveAgainst: List[MoveType] = List(Flying)
+}
+
+case object Flying extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Grass, Fighting, Bug)
+  override val resistedBy: List[MoveType] = List(Electric, Rock, Steel)
+  override val ineffectiveAgainst: List[MoveType] = List()
+}
+
+case object Psychic extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Fighting, Poison)
+  override val resistedBy: List[MoveType] = List(Psychic, Steel)
+  override val ineffectiveAgainst: List[MoveType] = List(Dark)
+}
+
+case object Bug extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Grass, Psychic, Dark)
+  override val resistedBy: List[MoveType] =
+    List(Fire, Fighting, Poison, Flying, Ghost, Steel, Fairy)
+  override val ineffectiveAgainst: List[MoveType] = List()
+}
+
+case object Rock extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Fire, Ice, Flying, Bug)
+  override val resistedBy: List[MoveType] = List(Fighting, Ground, Steel)
+  override val ineffectiveAgainst: List[MoveType] = List()
+}
+
+case object Ghost extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Ghost, Psychic)
+  override val resistedBy: List[MoveType] = List(Dark)
+  override val ineffectiveAgainst: List[MoveType] = List(Normal)
+}
+
+case object Dragon extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Dragon)
+  override val resistedBy: List[MoveType] = List(Steel)
+  override val ineffectiveAgainst: List[MoveType] = List(Fairy)
+}
+
+case object Dark extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Ghost, Psychic)
+  override val resistedBy: List[MoveType] = List(Fighting, Dark, Fairy)
+  override val ineffectiveAgainst: List[MoveType] = List()
+}
+
+case object Steel extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Ice, Rock, Fairy)
+  override val resistedBy: List[MoveType] = List(Fire, Water, Electric, Steel)
+  override val ineffectiveAgainst: List[MoveType] = List()
+}
+
+case object Fairy extends TypeImpl {
+  override val effectiveAgainst: List[MoveType] = List(Fighting, Dragon, Dark)
+  override val resistedBy: List[MoveType] = List(Fire, Poison, Steel)
+  override val ineffectiveAgainst: List[MoveType] = List()
 }
--- src/test/scala/it/unibo/skalamon/types/TypesTest.scala
+++ src/test/scala/it/unibo/skalamon/types/TypesTest.scala
@@ -4,32 +4,46 @@ import it.unibo.skalamon.model.types.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
-type PokemonType = (Type, Type) | Type
-type MoveType = Type
+case class TestMutablePokemon(var _hp: Int, _type: PokemonType)
+case class TestMove(_attack: Int, _type: MoveType)
 
-case class MutablePokemon(var _hp: Int, _type: PokemonType)
-case class Move(_attack: Int, _type: MoveType)
-
-// extension (pokemon: MutablePokemon)
-  // def attack(move: Move)(other: MutablePokemon): Unit = other._hp -= move._attack
+extension (pokemon: TestMutablePokemon)
+  def attack(move: TestMove)(other: TestMutablePokemon): Unit =
+    other._hp = other._hp - (move._attack * move._type.computeEffectiveness(
+      other._type
+    )).toInt
 
 /** Test for types.
   */
 class TypesTest extends AnyFlatSpec with should.Matchers {
 
   "Pokémon" should "have one type" in {
-    val pikachu: MutablePokemon = MutablePokemon(100, Electric)
+    val pikachu: TestMutablePokemon = TestMutablePokemon(100, Electric)
     pikachu._type shouldBe Electric
   }
 
   it should "can have two types" in {
-    val squirrel: MutablePokemon = MutablePokemon(120, (Electric, Water))
+    val squirrel: TestMutablePokemon =
+      TestMutablePokemon(120, (Electric, Water))
     squirrel._type match {
       case (t1: Type, t2: Type) =>
         t1 shouldBe Electric
         t2 shouldBe Water
       case t: Type => t.isInstanceOf[Type]
     }
+  }
+
+  "Moves" should "have one type" in {
+    val light: TestMove = TestMove(10, Electric)
+    light._type shouldBe Electric
+  }
+
+  "Electric move" should "be resisted on Electric Pokémon" in {
+    val pikachu: TestMutablePokemon = TestMutablePokemon(100, Electric)
+    val pikachu2: TestMutablePokemon = TestMutablePokemon(100, Electric)
+    val light: TestMove = TestMove(10, Electric)
+    pikachu.attack(light)(pikachu2)
+    pikachu2._hp shouldBe (95)
   }
 
 }
