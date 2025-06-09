45a65d224e7ab625f82a46f8ed2e7d18cb1ff465
Norbert Gabos <dont.vip@gmail.com>
2025-06-09 16:59:14 +0200

    implement Status traits
    also:
     - NonVolatileStatus
     - VolatileStatus

--- src/main/scala/it/unibo/skalamon/model/pokemon/Pokemon.scala
+++ src/main/scala/it/unibo/skalamon/model/pokemon/Pokemon.scala
@@ -1,22 +1,23 @@
 package it.unibo.skalamon.model.pokemon
 
 import it.unibo.skalamon.model.move.*
+import it.unibo.skalamon.model.status.*
 
 /** Represents the Stats of a Pokémon.
-  *
-  * @param hp
-  *   The Health Points of a Pokémon.
-  * @param attack
-  *   The attack of a Pokémon.
-  * @param defense
-  *   The defense of a Pokémon.
-  * @param specialAttack
-  *   The specialAttack of a Pokémon.
-  * @param specialDefense
-  *   The specialDefense of a Pokémon.
-  * @param speed
-  *   The attack of a Pokémon.
-  */
+ *
+ * @param hp
+ *   The Health Points of a Pokémon.
+ * @param attack
+ *   The attack of a Pokémon.
+ * @param defense
+ *   The defense of a Pokémon.
+ * @param specialAttack
+ *   The specialAttack of a Pokémon.
+ * @param specialDefense
+ *   The specialDefense of a Pokémon.
+ * @param speed
+ *   The attack of a Pokémon.
+ */
 case class Stats(
                   hp: Int,
                   attack: Int,
@@ -35,44 +36,44 @@ case class Type(name: String) //TODO: to be removed, it is temporary till merge
 case class Ability(name: String) //TODO: to be removed, it is temporary till merge
 
 /** Represents the base attributes of a Pokémon.
-  * @param name
-  *   The name of the Pokémon.
-  * @param gender
-  *   The gender of the Pokémon.
-  * @param types
-  *   The list of types of the Pokémon.
-  * @param baseStats
-  *   The base stats of the Pokémon.
-  * @param ability
-  *   The ability of the Pokémon.
-  * @param weightKg
-  *   The weight of the Pokémon in Kg.
-  * @param possibleMoves
-  *   The list of possible moves of the Pokémon.
+ * @param name
+ *   The name of the Pokémon.
+ * @param gender
+ *   The gender of the Pokémon.
+ * @param types
+ *   The list of types of the Pokémon.
+ * @param baseStats
+ *   The base stats of the Pokémon.
+ * @param ability
+ *   The ability of the Pokémon.
+ * @param weightKg
+ *   The weight of the Pokémon in Kg.
+ * @param possibleMoves
+ *   The list of possible moves of the Pokémon.
  */
-case class Pokemon(
-                        name: String,
+case class Pokemon(     name: String,
                         gender: Gender,
                         types: List[Type],
                         baseStats: Stats,
                         ability: Ability,
                         weightKg: Double,
                         possibleMoves: List[Move]
-                      )
+                  )
 
 case class BattlePokemon(
-                            base: Pokemon,
-                            level: Int,
-                            currentHP: Int,
-                            moves: List[BattleMove]
-                          ):
+                          base: Pokemon,
+                          level: Int,
+                          currentHP: Int,
+                          moves: List[BattleMove],
+                          nonVolatileStatus: Option[NonVolatileStatus],
+                          volatileStatus: List[VolatileStatus]
+                        ):
+
   def actualStats: Stats = calculateStats(base.baseStats, level)
 
-  //TODO: nonvolatilestatus: Option[Status], volitile: List[Status], trait Status, trait nonvolatileStatus, trait volatileStatus
-  
   private def calculateStats(base: Stats, level: Int): Stats =
     def scale(stat: Int): Int = (stat * level / 100.0).round.toInt + 5
-    
+
     Stats(
       hp = (base.hp * level / 100.0).round.toInt + level + 10,
       attack = scale(base.attack),
@@ -81,8 +82,8 @@ case class BattlePokemon(
       specialDefense = scale(base.specialDefense),
       speed = scale(base.speed)
     )
-  
+
   def isAlive: Boolean = currentHP > 0
-  
+
   def takeDamage(damage: Int): BattlePokemon =
     this.copy(currentHP = math.max(0, currentHP - damage))
