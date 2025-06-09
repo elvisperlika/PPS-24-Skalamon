5755ac39ff1ad380f225e128a227306fd97fbf04
noodles <elvis.perlika@studio.unibo.it>
2025-06-09 12:00:35 +0200

    feat: add Controller basic trait

--- src/main/scala/it/unibo/skalamon/controller/Controller.scala
+++ src/main/scala/it/unibo/skalamon/controller/Controller.scala
@@ -1,19 +1,16 @@
 package it.unibo.skalamon.controller
 
 import it.unibo.skalamon.controller.battle.BattleController
-import it.unibo.skalamon.view.View
 
 /** Controller
   */
 trait Controller {
 
-  /** Create a battle with 2 trainers, both of them with a team of Pokémon
+  /** Create a battle with 2 trainers, both of them with a team of Pokémon.
+    *
     * @return
     *   BattleController to manage the battle
     */
   def createBattle(): BattleController
-  
-  def run(): Unit
-  
-  def attachView(view: View): Unit 
+
 }
