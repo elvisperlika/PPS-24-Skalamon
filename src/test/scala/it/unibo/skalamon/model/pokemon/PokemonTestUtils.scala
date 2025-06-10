35bb21a6f626187f093bf972980fc9ca1d2d7296
Norbert Gabos <dont.vip@gmail.com>
2025-06-10 17:14:37 +0200

    fixed test + fixed formatting

--- src/test/scala/it/unibo/skalamon/model/pokemon/PokemonTestUtils.scala
+++ src/test/scala/it/unibo/skalamon/model/pokemon/PokemonTestUtils.scala
@@ -3,13 +3,19 @@ package it.unibo.skalamon.model.pokemon
 import it.unibo.skalamon.model.pokemon.*
 import it.unibo.skalamon.model.move.*
 
-/** Test utilities for testing pokemon.
- */
+/** Test utilities for testing Pokémon.
+  */
 object PokemonTestUtils:
   private val moveThunderShock = Move("Thunder Shock")
   private val moveElectric = Move("Electric")
-  
-  private val basePokemon = Pokemon("pikachu",
+
+  private val startingHP: Int = 70
+  private val powerPoint: Int = 4
+  private val levelPokemon1: Int = 100
+  private val levelPokemon2: Int = 86
+
+  private val basePokemon1 = Pokemon(
+    "Pikachu",
     Male,
     List(Type("Electric")),
     baseStats = Stats(35, 55, 40, 50, 50, 90),
@@ -17,7 +23,29 @@ object PokemonTestUtils:
     weightKg = 6.0,
     possibleMoves = List(moveThunderShock, moveElectric)
   )
-  private val startingHP: Int = 70
-  private val powerPoint: Int = 4
-  val simplePokemon1: BattlePokemon = BattlePokemon(basePokemon, 100, startingHP, List(BattleMove(moveThunderShock, powerPoint)))
-  val simplePokemon2: BattlePokemon = BattlePokemon(basePokemon, 86, startingHP, List(BattleMove(moveElectric, powerPoint)))
+  val simplePokemon1: BattlePokemon = BattlePokemon(
+    basePokemon1,
+    levelPokemon1,
+    startingHP,
+    List(BattleMove(moveThunderShock, powerPoint)),
+    Option.empty,
+    List()
+  )
+
+  private val basePokemon2 = Pokemon(
+    "Bulbasaur",
+    Male,
+    List(Type("Electric")),
+    baseStats = Stats(35, 55, 40, 50, 50, 90),
+    ability = Ability("Static"),
+    weightKg = 6.0,
+    possibleMoves = List(moveThunderShock, moveElectric)
+  )
+  val simplePokemon2: BattlePokemon = BattlePokemon(
+    basePokemon2,
+    levelPokemon2,
+    startingHP,
+    List(BattleMove(moveElectric, powerPoint)),
+    Option.empty,
+    List()
+  )
