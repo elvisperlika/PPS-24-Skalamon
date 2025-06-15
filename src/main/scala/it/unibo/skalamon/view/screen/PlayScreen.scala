e06b798fface2c9df94770c25256027278c2ead5
Norbert Gabos <dont.vip@gmail.com>
2025-06-15 09:05:45 +0200

    add player 2 + refactoring

--- src/main/scala/it/unibo/skalamon/view/screen/PlayScreen.scala
+++ src/main/scala/it/unibo/skalamon/view/screen/PlayScreen.scala
@@ -1,7 +1,6 @@
 package it.unibo.skalamon.view.screen
 
 import it.unibo.skalamon.view.Container.*
-
 import java.awt.Color
 import asciiPanel.AsciiPanel
 import java.awt.event.KeyEvent
@@ -16,12 +15,63 @@ class PlayScreen(
       player: String,
       opponent: String
   ): Unit =
-    terminal.writeCenter(opponent, 1)
-    terminal.writeCenter(player, 22)
-    HorizontalContainer(terminal, 3, 4, 12, 5)
+    import PlayScreen.*
+
+    terminal.writeCenter(opponent, opponentNameY)
+    terminal.writeCenter(player, playerNameY)
+
+    val p1Pokemon = HorizontalContainer(
+      terminal,
+      startY,
+      pokemonSlotNum,
+      pokemonSlotWidth,
+      pokemonSlotHeight
+    )
+    val p1Abilities = HorizontalContainer(
+      terminal,
+      startY + pokemonSlotHeight,
+      abilitySlotNum,
+      abilitySlotWidth,
+      abilitySlotHeight
+    )
+
+    val p2AbilitiesY =
+      startY + pokemonSlotHeight + abilitySlotHeight + paddingBetweenPlayers
+    val p2PokemonY = p2AbilitiesY + abilitySlotHeight
+
+    val p2Abilities = HorizontalContainer(
+      terminal,
+      p2AbilitiesY,
+      abilitySlotNum,
+      abilitySlotWidth,
+      abilitySlotHeight
+    )
+    val p2Pokemon = HorizontalContainer(
+      terminal,
+      p2PokemonY,
+      pokemonSlotNum,
+      pokemonSlotWidth,
+      pokemonSlotHeight
+    )
 
   override def displayOutput(terminal: AsciiPanel): Unit =
     setPlayersName(terminal, playerName, opponentName)
 
   override def respondToUserInput(key: KeyEvent): Screen =
     this
+
+object PlayScreen:
+  val opponentNameY = 1
+  val playerNameY = 22
+
+  val startY = 3
+
+  val pokemonSlotNum = 5
+  val pokemonSlotWidth = 12
+  val pokemonSlotHeight = 3
+
+  val abilitySlotNum = 4
+  val abilitySlotWidth = 16
+  val abilitySlotHeight = 5
+
+  val paddingBetweenPlayers = 1
