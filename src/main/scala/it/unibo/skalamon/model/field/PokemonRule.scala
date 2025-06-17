0137887f65a83f40bda49df113a92aa1d736d71f
noodles <elvis.perlika@studio.unibo.it>
2025-06-17 17:28:22 +0200

    feat: weather modifier is more generic

--- src/main/scala/it/unibo/skalamon/model/field/PokemonRule.scala
+++ src/main/scala/it/unibo/skalamon/model/field/PokemonRule.scala
@@ -2,30 +2,25 @@ package it.unibo.skalamon.model.field
 
 import it.unibo.skalamon.model.pokemon.BattlePokemon
 import it.unibo.skalamon.model.types.Type
-import it.unibo.skalamon.model.types.TypesCollection.Ice
+import it.unibo.skalamon.model.types.TypesCollection.{Electric, Ice}
 
 trait PokemonRule:
   def apply(p: BattlePokemon): BattlePokemon
-
-trait Filter:
-  val filter: FilterEnum
-  val types: List[Type]
-
-  def shouldApplyTo(p: BattlePokemon): Boolean = filter match
-    case FilterEnum.All    => types.contains(p.base.types)
-    case FilterEnum.Except => !types.contains(p.base.types)
-
-trait Damage:
-  val damage: Int
 
 enum FilterEnum:
   case All
   case Except
 
-object DamageAllExceptIce extends PokemonRule with Filter with Damage:
-  override def apply(p: BattlePokemon): BattlePokemon =
-    if shouldApplyTo(p) then p.copy(currentHP = p.currentHP - damage) else p
+object Modify:
+  def only(types: Type*): ModifyBuilder =
+    ModifyBuilder(FilterEnum.All, types.toList)
+  def except(types: Type*): ModifyBuilder =
+    ModifyBuilder(FilterEnum.Except, types.toList)
 
-  override val filter: FilterEnum = FilterEnum.Except
-  override val types: List[Type] = Ice :: Nil
-  override val damage: Int = 10
+case class ModifyBuilder(filter: FilterEnum, types: List[Type]):
+  def apply(modification: BattlePokemon => BattlePokemon): PokemonRule =
+    (p: BattlePokemon) =>
+      val matches = filter match
+        case FilterEnum.All    => types.contains(p.base.types)
+        case FilterEnum.Except => !types.contains(p.base.types)
+      if matches then modification(p) else p
--- src/main/scala/it/unibo/skalamon/model/field/weather/kind/Snow.scala
+++ src/main/scala/it/unibo/skalamon/model/field/weather/kind/Snow.scala
@@ -1,15 +1,22 @@
 package it.unibo.skalamon.model.field.weather.kind
 
+import it.unibo.skalamon.model.field.{Expirable, Modify, PokemonRule}
 import it.unibo.skalamon.model.field.weather.WhenAffect.OnCreation
 import it.unibo.skalamon.model.field.weather.{Weather, WhenAffect}
-import it.unibo.skalamon.model.field.{DamageAllExceptIce, Expirable, PokemonRule}
+import it.unibo.skalamon.model.move.BattleMove
 import it.unibo.skalamon.model.types.Type
+import it.unibo.skalamon.model.types.TypesCollection.Ice
 
-case class Snow(name: String, turn: Int, elapsedTurn: Int, whenAffect: WhenAffect)
-    extends Weather(name, whenAffect) with Expirable(turn, elapsedTurn):
+case class Snow(
+    name: String,
+    turn: Int,
+    elapsedTurn: Int,
+    whenAffect: WhenAffect
+) extends Weather(name, whenAffect) with Expirable(turn, elapsedTurn):
 
   override val typesMultiplierMap: Map[Type, Double] = Map.empty
-  override val fieldEffects: List[PokemonRule] = DamageAllExceptIce :: Nil
+  override val fieldEffects: List[PokemonRule] =
+    Modify.except(Ice) { p => p.copy(currentHP = p.currentHP - 10) } :: Nil
 
 object Snow:
   def apply(t: Int): Snow = Snow("Snow", t, 5, OnCreation)
