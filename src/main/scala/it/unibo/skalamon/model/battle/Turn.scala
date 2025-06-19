5fe6a3e66074854e154bd9827db088c068d30abb
Giorgio Garofalo <giogar2705@gmail.com>
2025-06-19 19:06:46 +0200

    docs: update TurnStage documentation

--- src/main/scala/it/unibo/skalamon/model/battle/Turn.scala
+++ src/main/scala/it/unibo/skalamon/model/battle/Turn.scala
@@ -23,19 +23,41 @@ case class TurnState(
     stage: TurnStage
 )
 
-/** The different stages a turn can be in.
-  *
-  *   - `Started`: The turn has just started.
-  *   - `WaitingForActions`: Waiting for trainers to register their actions.
-  *   - `ActionsReceived`: Actions have been received from all trainers.
-  *   - `ExecutingActions`: Actions are being executed.
-  *   - `Ended`: The turn has ended and is ready for the next one.
-  */
+/** The different stages a turn can be in. */
 enum TurnStage:
+  /** The turn has just started.
+    *
+    * Here, abilities can be activated and weather effects can be applied.
+    */
   case Started
+
+  /** Waiting for trainers to register their actions.
+    *
+    * Here, trainers can register their actions for the turn.
+    */
   case WaitingForActions
+
+  /** Actions have been received from all trainers.
+    *
+    * Here, the actions are collected in an [[ActionBuffer]] and the turn is
+    * ready to execute them.
+    *
+    * @param actionBuffer
+    *   The buffer containing the actions registered by trainers.
+    */
   case ActionsReceived(actionBuffer: ActionBuffer)
+
+  /** Actions are being executed.
+    *
+    * Here, the actions registered by trainers, anwith their behaviors, are
+    * executed according to priority and speed.
+    */
   case ExecutingActions
+
+  /** The turn has ended and is ready for the next one.
+    *
+    * Here, abilities can be activated, and expirable effects can decrease.
+    */
   case Ended
 
 object TurnState:
