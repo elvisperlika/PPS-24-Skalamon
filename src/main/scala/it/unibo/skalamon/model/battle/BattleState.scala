41b29c71eef8cccd51e7a0c7fe1d0ca690215c67
Giorgio Garofalo <giogar2705@gmail.com>
2025-06-22 17:55:48 +0200

    feat: add battle state updates

--- src/main/scala/it/unibo/skalamon/model/battle/BattleState.scala
+++ src/main/scala/it/unibo/skalamon/model/battle/BattleState.scala
@@ -1,7 +1,9 @@
 package it.unibo.skalamon.model.battle
 
-/** */
+/** The immutable state of a battle, containing all the trainers involved, their
+  * Pokémon, and other relevant battle conditions.
+  */
 case class BattleState(
-    trainers: List[Trainer],
+    trainers: List[Trainer]
     // TODO field, weather, terrain, room, side conditions, etc.
 )
