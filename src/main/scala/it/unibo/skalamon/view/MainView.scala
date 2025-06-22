153296b94d00007380eecc20383c6d2126785a8a
Norbert Gabos <dont.vip@gmail.com>
2025-06-22 22:18:02 +0200

    refactor MainView to simplify initialization

--- src/main/scala/it/unibo/skalamon/view/MainView.scala
+++ src/main/scala/it/unibo/skalamon/view/MainView.scala
@@ -6,33 +6,23 @@ import javax.swing.{JFrame, WindowConstants}
 import java.awt.event.{KeyEvent, KeyListener}
 import asciiPanel.AsciiPanel
 
-class MainView extends View, KeyListener:
-  private var terminal: Option[AsciiPanel] = None
-  private var screen: Option[Screen] = None
-
+class MainView() extends JFrame:
   private val terminalWidth: Int = 80
   private val terminalHeight: Int = 35
 
-  def createView(): Unit =
-    terminal = Option(AsciiPanel(terminalWidth, terminalHeight))
+  val terminal: AsciiPanel = AsciiPanel(terminalWidth, terminalHeight)
+
+  def setupView(): Unit =
     setResizable(false)
-    add(terminal.get)
+    add(terminal)
     pack()
 
-    // screen = Option(MainMenuScreen())
-    screen = Option(PlayScreen("Fabrizio", "Edgar"))
-    addKeyListener(this)
-    repaint()
     this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
     this.setVisible(true)
 
-  override def repaint(): Unit =
-    terminal.get.clear
-    screen.get.displayOutput(terminal.get)
-    super.repaint()
+  def getPlayScreen(): BattleScreen =
+    BattleScreen(terminal)
 
-  override def keyPressed(e: KeyEvent): Unit =
-    screen = Option(screen.get.respondToUserInput(e))
-    repaint
-  override def keyReleased(e: KeyEvent): Unit = {}
-  override def keyTyped(e: KeyEvent): Unit = {}
+  override def repaint(): Unit =
+    terminal.clear
+    super.repaint()
