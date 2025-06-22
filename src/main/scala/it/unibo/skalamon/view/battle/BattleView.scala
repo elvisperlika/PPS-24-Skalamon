e00ed143d8084d0e13de3be716b209954eea3d29
Norbert Gabos <dont.vip@gmail.com>
2025-06-22 22:22:01 +0200

    show trainers name

--- src/main/scala/it/unibo/skalamon/view/battle/BattleView.scala
+++ src/main/scala/it/unibo/skalamon/view/battle/BattleView.scala
@@ -1,8 +1,10 @@
 package it.unibo.skalamon.view.battle
 
+import asciiPanel.AsciiPanel
 import it.unibo.skalamon.controller.battle.action.*
 import it.unibo.skalamon.model.battle.Trainer
 import it.unibo.skalamon.model.pokemon.BattlePokemon
+import it.unibo.skalamon.view.screen.BattleScreen
 
 trait BattleView:
 
@@ -24,18 +26,27 @@ trait BattleView:
     */
   def updatePokemon(map: Map[Trainer, BattlePokemon]): Unit
 
-object BattleView {
-  def apply(controllerProxy: ActionBuffer): BattleView =
-    new BattleViewImpl(controllerProxy)
+object BattleView:
+  def apply(screen: BattleScreen, controllerProxy: ActionBuffer): BattleView =
+    new BattleViewImpl(screen, controllerProxy)
 
-  private class BattleViewImpl(_controllerProxy: ActionBuffer)
-      extends BattleView {
+  private class BattleViewImpl(
+      screen: BattleScreen,
+      _controllerProxy: ActionBuffer
+  ) extends BattleView:
     private val controllerProxy = _controllerProxy
 
     override def updateTurn(i: Int): Unit = ???
 
     override def showActions(map: Map[Trainer, List[Action]]): Unit = ???
 
-    override def updatePokemon(map: Map[Trainer, BattlePokemon]): Unit = ???
-  }
-}
+    override def updatePokemon(map: Map[Trainer, BattlePokemon]): Unit =
+      val trainers = map.keys.toList
+      if (trainers.size != screen.playerNumber) {
+        throw new IllegalArgumentException(
+          s"Expected ${screen.playerNumber} trainers, but got ${trainers.size}."
+        )
+      }
+      val player = trainers(0)
+      val opponent = trainers(1)
+      screen.setPlayersName(player.name, opponent.name)
