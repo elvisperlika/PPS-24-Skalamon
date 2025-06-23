e485f077fa35fecd33b35a846b5d36a8677a8264
Norbert Gabos <dont.vip@gmail.com>
2025-06-23 17:38:17 +0200

    add tests for stat changes in BattlePokemon

--- src/test/scala/it/unibo/skalamon/model/pokemon/BattlePokemonTest.scala
+++ src/test/scala/it/unibo/skalamon/model/pokemon/BattlePokemonTest.scala
@@ -1,5 +1,6 @@
 package it.unibo.skalamon.model.pokemon
 
+import it.unibo.skalamon.model.behavior.kind.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
@@ -18,6 +19,19 @@ class BattlePokemonTest extends AnyFlatSpec with should.Matchers:
       .takeDamage(damage1)
       .takeDamage(damage2)
       .currentHP shouldEqual (pokemon.currentHP - damage1 - damage2)
+
+  "BattlePokemon" should "apply stat changes and update effective stat value" in:
+    val pokemon = PokemonTestUtils.simplePokemon1
+    val stat = Stat.Attack
+    val baseValue = pokemon.base.baseStats.base(stat)
+
+    pokemon.actualStats.effective(stat) shouldEqual baseValue
+
+    val changed = pokemon.applyStatChange(StatChange(stat, 2))
+    changed.actualStats.effective(stat) shouldEqual baseValue * 2.0
+
+    val changedAgain = changed.applyStatChange(StatChange(stat, -4))
+    changedAgain.actualStats.effective(stat) shouldEqual baseValue * 0.5
 
   "BattlePokemon" should "die" in:
     val pokemon = PokemonTestUtils.simplePokemon1
