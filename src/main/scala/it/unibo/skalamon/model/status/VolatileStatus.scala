11ca847716474dfb60990486119e70f8bb780d07
Norbert Gabos <dont.vip@gmail.com>
2025-06-23 17:53:43 +0200

    remove unused volatile statuses and update tests

--- src/main/scala/it/unibo/skalamon/model/status/VolatileStatus.scala
+++ src/main/scala/it/unibo/skalamon/model/status/VolatileStatus.scala
@@ -6,28 +6,8 @@ case object Confusion extends VolatileStatus
 
 case object Flinch extends VolatileStatus
 
-case object LeechSeed extends VolatileStatus
-
-case object PerishSong extends VolatileStatus
-
 case object Yawn extends VolatileStatus
-
-case object BindMove extends VolatileStatus
-
-case object AquaRingIngrain extends VolatileStatus
-
-case object Encore extends VolatileStatus
-
-case object Taunt extends VolatileStatus
-
-case object Torment extends VolatileStatus
-
-case object Disable extends VolatileStatus
-
-case object Trapped extends VolatileStatus
 
 case object ProtectEndure extends VolatileStatus
 
 case object Substitute extends VolatileStatus
-
-case object FocusEnergy extends VolatileStatus
