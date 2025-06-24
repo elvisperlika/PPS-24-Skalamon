f30e00f6a5eae6a97fffab7a1e6953e71119ee7b
noodles <elvis.perlika@studio.unibo.it>
2025-06-24 18:57:35 +0200

    refactor: extract method to check game over

--- src/main/scala/it/unibo/skalamon/model/event/BattleConfiguration.scala
+++ src/main/scala/it/unibo/skalamon/model/event/BattleConfiguration.scala
@@ -1,16 +1,18 @@
 package it.unibo.skalamon.model.event
 
+import it.unibo.skalamon.model.battle.Turn
 import it.unibo.skalamon.model.event.BattleStateEvents.Finished
 import it.unibo.skalamon.model.event.TurnStageEvents.TurnEnded
 
 trait BattleConfiguration extends EventManager:
   watch(TurnEnded) { t =>
-    val aliveTrainers =
-      t.state.snapshot.trainers.filter(_.team.exists(_.isAlive))
-    aliveTrainers match
-      case Seq(winner) =>
-        notify(Finished of Some(winner))
-      case Seq() =>
-        notify(Finished of None)
-      case _ => (false, None)
+    checkGameOver(t)
   }
+
+  private def checkGameOver(turn: Turn): Unit =
+    val aliveTrainers =
+      turn.state.snapshot.trainers.filter(_.team.exists(_.isAlive))
+    aliveTrainers match
+      case Seq(winner) => notify(Finished of Some(winner))
+      case Seq()       => notify(Finished of None)
+      case _           => ()
