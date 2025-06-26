8849b1137ccf51c79aef887aec83b389b6b5d03a
Norbert Gabos <dont.vip@gmail.com>
2025-06-26 18:55:30 +0200

    moved BattlePokemon to it's file

---
+++ src/main/scala/it/unibo/skalamon/model/pokemon/BattlePokemon.scala
@@ -0,0 +1,71 @@
+package it.unibo.skalamon.model.pokemon
+
+import it.unibo.skalamon.model.behavior.kind.{StatChange, Stats}
+import it.unibo.skalamon.model.move.BattleMove
+import it.unibo.skalamon.model.status.{
+  AssignedStatus,
+  NonVolatileStatus,
+  VolatileStatus
+}
+
+import java.util.UUID
+
+/** Represents the whole Pokémon.
+  *
+  * @param base
+  *   The base attributes of the Pokémon.
+  * @param gender
+  *   The gender of the Pokémon.
+  * @param level
+  *   The level of the Pokémon.
+  * @param currentHP
+  *   Current Health Points of the Pokémon.
+  * @param moves
+  *   List of moves of the Pokémon.
+  * @param nonVolatileStatus
+  *   The non-volatile status of the Pokémon.
+  * @param volatileStatus
+  *   Set of volatile status of the Pokémon.
+  * @param id
+  *   The unique identifier of the battle Pokémon. This is useful to track the
+  *   Pokémon in a battle context: even if this instance is copied, the ID
+  *   remains the same and can be tracked across different states.
+  */
+case class BattlePokemon(
+    base: Pokemon,
+    gender: Gender,
+    level: Int,
+    currentHP: Int,
+    moves: List[BattleMove],
+    nonVolatileStatus: Option[AssignedStatus[NonVolatileStatus]],
+    volatileStatus: Set[AssignedStatus[VolatileStatus]],
+    id: UUID = UUID.randomUUID()
+):
+
+  /** Return the current stats of the Pokémon, updated to it's level.
+    * @return
+    *   the current stats of the Pokémon.
+    */
+  def actualStats: Stats =
+    base.baseStats // TODO: for now, just return the base stats
+
+  /** Return true if the Pokémon is still alive.
+    * @return
+    *   true if the Pokémon is still alive.
+    */
+  def isAlive: Boolean = currentHP > 0
+
+  /** Deals damage to the Pokémon.
+    *
+    * @param damage
+    *   The damage to be inflicted on the Pokémon.
+    * @return
+    *   the copy of the damaged Pokémon.
+    */
+  def takeDamage(damage: Int): BattlePokemon =
+    this.copy(currentHP =
+      math.max(0, currentHP - damage)
+    ) // TODO: this method is just temporary, to be removed when the battle engine is implemented
+
+  def applyStatChange(change: StatChange): BattlePokemon =
+    this.copy(base = base.copy(baseStats = base.baseStats.applyChange(change)))
--- src/main/scala/it/unibo/skalamon/model/pokemon/Pokemon.scala
+++ src/main/scala/it/unibo/skalamon/model/pokemon/Pokemon.scala
@@ -3,10 +3,8 @@ package it.unibo.skalamon.model.pokemon
 import it.unibo.skalamon.model.ability.*
 import it.unibo.skalamon.model.behavior.kind.*
 import it.unibo.skalamon.model.move.*
-import it.unibo.skalamon.model.status.*
 import it.unibo.skalamon.model.types.*
-
-import java.util.UUID
+import it.unibo.skalamon.model.types.TypesCollection.Electric
 
 sealed trait Gender
 case object Male extends Gender
@@ -56,41 +54,6 @@ case class Pokemon(
   *   Pokémon in a battle context: even if this instance is copied, the ID
   *   remains the same and can be tracked across different states.
   */
-case class BattlePokemon(
-    base: Pokemon,
-    gender: Gender,
-    level: Int,
-    currentHP: Int,
-    moves: List[BattleMove],
-    nonVolatileStatus: Option[AssignedStatus[NonVolatileStatus]],
-    volatileStatus: Set[AssignedStatus[VolatileStatus]],
-    id: UUID = UUID.randomUUID()
-):
-
-  /** Return the current stats of the Pokémon, updated to it's level.
     * @return
-    *   the current stats of the Pokémon.
+    *   A `Pokemon` instance representing the Pokémon.
     */
-  def actualStats: Stats =
-    base.baseStats // TODO: for now, just return the base stats
-
-  /** Return true if the Pokémon is still alive.
-    * @return
-    *   true if the Pokémon is still alive.
-    */
-  def isAlive: Boolean = currentHP > 0
-
-  /** Deals damage to the Pokémon.
-    *
-    * @param damage
-    *   The damage to be inflicted on the Pokémon.
-    * @return
-    *   the copy of the damaged Pokémon.
-    */
-  def takeDamage(damage: Int): BattlePokemon =
-    this.copy(currentHP =
-      math.max(0, currentHP - damage)
-    ) // TODO: this method is just temporary, to be removed when the battle engine is implemented
-
-  def applyStatChange(change: StatChange): BattlePokemon =
-    this.copy(base = base.copy(baseStats = base.baseStats.applyChange(change)))
