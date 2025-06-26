fae960a1b01327d831731f084fe77e07562f29d0
noodles <elvis.perlika@studio.unibo.it>
2025-06-26 09:09:01 +0200

    feat: add ordering by Pokémon speed and Move priority

--- src/main/scala/it/unibo/skalamon/model/event/BattleConfiguration.scala
+++ src/main/scala/it/unibo/skalamon/model/event/BattleConfiguration.scala
@@ -3,7 +3,12 @@ package it.unibo.skalamon.model.event
 import it.unibo.skalamon.controller.battle.action.{MoveAction, SwitchAction}
 import it.unibo.skalamon.model.battle.Turn
 import it.unibo.skalamon.model.battle.TurnStage.ActionsReceived
-import it.unibo.skalamon.model.behavior.kind.HealthBehavior
+import it.unibo.skalamon.model.behavior.kind.{
+  HealthBehavior,
+  SingleHitBehavior,
+  StatChangeBehavior,
+  StatusBehavior
+}
 import it.unibo.skalamon.model.behavior.visitor.BattleStateUpdaterBehaviorVisitor
 import it.unibo.skalamon.model.event.BattleStateEvents.Finished
 import it.unibo.skalamon.model.event.TurnStageEvents.{Ended, ExecutingActions}
@@ -22,29 +27,37 @@ trait BattleConfiguration extends EventManager:
       case _           => ()
 
   watch(ExecutingActions) { t =>
-    t.state.stage match
-      case ActionsReceived(actionBuffer) =>
-        val actions = t.state.snapshot.trainers.flatMap(actionBuffer.getAction)
-        val switches = actions.collect { case SwitchAction() =>
-          SwitchAction()
-        }
-        val moveActions = actions.collect { case MoveAction(m) => m }
-
-        import it.unibo.skalamon.model.move.MoveContext
-        implicit val moveOrdering: Ordering[MoveContext] =
-          Ordering.by(_.origin.move.priority)
-        val sortedMoves = moveActions.sorted
-
-        // TODO: execute all switches
-
-        var newState = t.state.snapshot
-        for move <- sortedMoves; (behavior, mod) <- move.behaviors do
-          val visitor = BattleStateUpdaterBehaviorVisitor(
-            current = newState,
-            context = move,
-            modifiers = mod
-          )
-          newState = behavior.accept(visitor)
-
-      case _ => ()
+    executeMoves(t)
   }
+
+  private def executeMoves(turn: Turn): Unit =
+    turn.state.stage match
+      case ActionsReceived(actionBuffer) =>
+        val actions =
+          turn.state.snapshot.trainers.flatMap(actionBuffer.getAction)
+        val switches = actions.collect { case SwitchAction() => SwitchAction() }
+        //TODO: execute switches
+        
+        val moveContexts = actions.collect { case MoveAction(m) => m }
+        import it.unibo.skalamon.model.move.MoveContext
+        val orderedPartitions: Map[Int, List[MoveContext]] =
+          moveContexts.groupBy(_.origin.move.priority).toSeq.sortBy(_._1).toMap
+
+        orderedPartitions.foreach { (_, moveContexts) =>
+          implicit val moveOrdering: Ordering[MoveContext] =
+            Ordering.by(_.origin.move.priority)
+          val sortedMoveContexts = moveContexts.sorted
+          var newState = turn.state.snapshot
+          for move <- sortedMoveContexts; (behavior, mod) <- move.behaviors do
+            val visitor = BattleStateUpdaterBehaviorVisitor(
+              current = newState,
+              context = move,
+              modifiers = mod
+            )
+            newState = behavior match
+              case b: HealthBehavior     => visitor.visit(b)
+              case b: SingleHitBehavior  => visitor.visit(b)
+              case b: StatChangeBehavior => visitor.visit(b)
+              case b: StatusBehavior     => visitor.visit(b)
+        }
+      case _ => ()
