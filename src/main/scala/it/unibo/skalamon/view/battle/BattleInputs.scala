f107039caa6ac7235820d4de2ab80a126e78deb1
Norbert Gabos <dont.vip@gmail.com>
2025-07-01 20:30:47 +0200

    create partial key mapping for BattleView

---
+++ src/main/scala/it/unibo/skalamon/view/battle/BattleInputs.scala
@@ -0,0 +1,19 @@
+package it.unibo.skalamon.view.battle
+
+import java.awt.event.KeyEvent
+
+enum BattleInput:
+  case playerPokemon1, playerPokemon2, playerPokemon3, playerPokemon4,
+    playerPokemon5
+  case playerAction1, playerAction2, playerAction3, playerAction4
+
+  case opponentPokemon1, opponentPokemon2, opponentPokemon3, opponentPokemon4,
+    opponentPokemon5
+  case opponentAction1, opponentAction2, opponentAction3, opponentAction4
+
+def keyEventToBattleInput(keyEvent: KeyEvent): Option[BattleInput] =
+  keyEvent.getKeyCode match
+
+    case KeyEvent.VK_Q => Some(BattleInput.playerAction1)
+    case KeyEvent.VK_Z => Some(BattleInput.opponentAction1)
+    case _ => None
