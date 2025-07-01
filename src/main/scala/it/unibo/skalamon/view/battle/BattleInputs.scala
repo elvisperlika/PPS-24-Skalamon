c0b306d6bbe7d4eadd289a99c65b0c07d683fa78
Norbert Gabos <dont.vip@gmail.com>
2025-07-01 21:09:23 +0200

    rename action to move

--- src/main/scala/it/unibo/skalamon/view/battle/BattleInputs.scala
+++ src/main/scala/it/unibo/skalamon/view/battle/BattleInputs.scala
@@ -5,22 +5,23 @@ import java.awt.event.KeyEvent
 enum BattleInput:
   case playerPokemon1, playerPokemon2, playerPokemon3, playerPokemon4,
     playerPokemon5
-  case playerAction1, playerAction2, playerAction3, playerAction4
+  case playerMove1, playerMove2, playerMove3, playerMove4
 
   case opponentPokemon1, opponentPokemon2, opponentPokemon3, opponentPokemon4,
     opponentPokemon5
-  case opponentAction1, opponentAction2, opponentAction3, opponentAction4
+  case opponentMove1, opponentMove2, opponentMove3, opponentMove4
 
 def keyEventToBattleInput(keyEvent: KeyEvent): Option[BattleInput] =
   keyEvent.getKeyCode match
 
-    case KeyEvent.VK_Q => Some(BattleInput.playerAction1)
-    case KeyEvent.VK_W => Some(BattleInput.playerAction2)
-    case KeyEvent.VK_E => Some(BattleInput.playerAction3)
-    case KeyEvent.VK_R => Some(BattleInput.playerAction4)
-    case KeyEvent.VK_Z => Some(BattleInput.opponentAction1)
-    case KeyEvent.VK_X => Some(BattleInput.opponentAction2)
-    case KeyEvent.VK_C => Some(BattleInput.opponentAction3)
-    case KeyEvent.VK_V => Some(BattleInput.opponentAction4)
+    case KeyEvent.VK_Q => Some(BattleInput.playerMove1)
+    case KeyEvent.VK_W => Some(BattleInput.playerMove2)
+    case KeyEvent.VK_E => Some(BattleInput.playerMove3)
+    case KeyEvent.VK_R => Some(BattleInput.playerMove4)
+
+    case KeyEvent.VK_Z => Some(BattleInput.opponentMove1)
+    case KeyEvent.VK_X => Some(BattleInput.opponentMove2)
+    case KeyEvent.VK_C => Some(BattleInput.opponentMove3)
+    case KeyEvent.VK_V => Some(BattleInput.opponentMove4)
 
     case _ => None
