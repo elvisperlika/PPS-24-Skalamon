63a1f196d21827353c3dfb0ecbc630991de8086f
Norbert Gabos <dont.vip@gmail.com>
2025-07-04 19:31:28 +0200

    add doc

--- src/main/scala/it/unibo/skalamon/model/status/NonVolatileStatus.scala
+++ src/main/scala/it/unibo/skalamon/model/status/NonVolatileStatus.scala
@@ -7,6 +7,15 @@ trait NonVolatileStatus extends Status:
   private val minPercent: Int = 0
   private val maxPercent: Int = 100
 
+  /** Attempts to skip the current turn based on a given probability.
+    * @param pokemon
+    *   The Pokémon whose turn may be skipped.
+    * @param probability
+    *   The chance (in percent) to skip the turn.
+    * @return
+    *   A copy of the Pokémon with the `skipsCurrentTurn` flag set if the turn
+    *   is skipped.
+    */
   def skipTurn(pokemon: BattlePokemon, probability: Int): BattlePokemon =
     val randomValue = RandomGenerator().nextInt(minPercent, maxPercent)
     if randomValue < probability then
@@ -14,6 +23,12 @@ trait NonVolatileStatus extends Status:
     else
       pokemon
 
+  /** Executes the effect of the non-volatile status on the given Pokémon.
+    * @param pokemon
+    *   The Pokémon to apply the status effect to.
+    * @return
+    *   A copy of the Pokémon with the status effect applied.
+    */
   def executeEffect(
       pokemon: BattlePokemon
   ): BattlePokemon
