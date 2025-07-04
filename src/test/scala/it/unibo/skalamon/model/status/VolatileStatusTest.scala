fff0b1d770ac25cbc8da0e311f0b815ee4a3a7ec
Norbert Gabos <dont.vip@gmail.com>
2025-07-04 19:46:29 +0200

    setup test for VolatileStatusTest

---
+++ src/test/scala/it/unibo/skalamon/model/status/VolatileStatusTest.scala
@@ -0,0 +1,9 @@
+package it.unibo.skalamon.model.status
+
+import it.unibo.skalamon.model.field.FieldEffectMixin.Expirable
+import it.unibo.skalamon.model.pokemon.BattlePokemon
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class VolatileStatusTest extends AnyFlatSpec with should.Matchers:
+  {}
