c28853a9cb40ddc49ce648bd2350c4c18778fe91
Norbert Gabos <dont.vip@gmail.com>
2025-07-04 19:19:01 +0200

    implement executeEffect for NonVolatileStatus

--- src/main/scala/it/unibo/skalamon/model/status/NonVolatileStatus.scala
+++ src/main/scala/it/unibo/skalamon/model/status/NonVolatileStatus.scala
@@ -1,13 +1,23 @@
 package it.unibo.skalamon.model.status
 
+import it.unibo.skalamon.model.data.RandomGenerator
 import it.unibo.skalamon.model.pokemon.{BattlePokemon, Stat}
 
 trait NonVolatileStatus extends Status:
+  private val minPercent: Int = 0
+  private val maxPercent: Int = 100
+
+  def skipTurn(pokemon: BattlePokemon, probability: Int): BattlePokemon =
+    val randomValue = RandomGenerator().nextInt(minPercent, maxPercent)
+    if randomValue < probability then
+      pokemon.copy(skipsCurrentTurn = true)
+    else
+      pokemon
+
   def executeEffect(
       pokemon: BattlePokemon
   ): BattlePokemon
 
-case object Paralyze extends NonVolatileStatus
 /** Inflicts damage equal to 1/16 of the maximum HP and halves the physical
   * Attack stat.
   */
@@ -15,9 +25,40 @@ case object Burn extends NonVolatileStatus:
   val BurnAttackReduction = 2
   val BurnDamageReduction = 16
 
-case object Sleep extends NonVolatileStatus
+  override def executeEffect(
+      pokemon: BattlePokemon
+  ): BattlePokemon =
+    val updatedStats = pokemon.base.baseStats.base.updatedWith(Stat.Attack) {
+      case Some(value) => Some(value / BurnAttackReduction)
+      case other       => other
+    }
+    pokemon.copy(
+      currentHP = pokemon.currentHP - (pokemon.base.hp / BurnDamageReduction),
+      base =
+        pokemon.base.copy(baseStats =
+          pokemon.base.baseStats.copy(base = updatedStats)
+        )
+    )
+
+/** Reduces Speed to 50% and has a 25% chance to skip the current turn. */
+case object Paralyze extends NonVolatileStatus:
+  val ParalyzeAttackReduction = 2
+  val ParalyzeTriggerChance = 25
 
 case object Freeze extends NonVolatileStatus
+  override def executeEffect(
+      pokemon: BattlePokemon
+  ): BattlePokemon =
+    val updatedStats = pokemon.base.baseStats.base.updatedWith(Stat.Speed) {
+      case Some(value) => Some(value / ParalyzeAttackReduction)
+      case other       => other
+    }
+    val updatedPokemon = pokemon.copy(
+      base = pokemon.base.copy(
+        baseStats = pokemon.base.baseStats.copy(base = updatedStats)
+      )
+    )
+    skipTurn(updatedPokemon, ParalyzeTriggerChance)
 
 case object Poison extends NonVolatileStatus
 
