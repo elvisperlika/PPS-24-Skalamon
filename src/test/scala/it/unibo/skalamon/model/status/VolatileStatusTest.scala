e8a5bf8d2d864dfa7fc4e7cf646134ef4732c078
Norbert Gabos <dont.vip@gmail.com>
2025-07-05 23:27:17 +0200

    test Flinch

--- src/test/scala/it/unibo/skalamon/model/status/VolatileStatusTest.scala
+++ src/test/scala/it/unibo/skalamon/model/status/VolatileStatusTest.scala
@@ -1,7 +1,11 @@
 package it.unibo.skalamon.model.status
 
 import it.unibo.skalamon.model.pokemon.{BattlePokemon, PokemonTestUtils}
-import it.unibo.skalamon.model.status.volatileStatus.{ProtectEndure, Yawn}
+import it.unibo.skalamon.model.status.volatileStatus.{
+  Flinch,
+  ProtectEndure,
+  Yawn
+}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
@@ -14,6 +18,8 @@ class VolatileStatusTest extends AnyFlatSpec with should.Matchers:
     AssignedStatus(Yawn(), initialTurn)
   private val assignedProtectEndure: AssignedStatus[VolatileStatus] =
     AssignedStatus(ProtectEndure(), initialTurn)
+  private val assignedFlinch: AssignedStatus[VolatileStatus] =
+    AssignedStatus(Flinch(), initialTurn)
 
   "Yawn" should "make the pokemon skip the next turn" in:
     val pokemon: BattlePokemon = PokemonTestUtils.simplePokemon4.copy(
@@ -36,9 +42,20 @@ class VolatileStatusTest extends AnyFlatSpec with should.Matchers:
     val pokemon: BattlePokemon = PokemonTestUtils.simplePokemon4.copy(
       volatileStatus = Set(assignedProtectEndure)
     )
-  
+
     pokemon.volatileStatus.head.status.executeEffect(
       pokemon,
       initialTurn,
       initialTurn
     ).isProtected shouldBe true
+
+  "Flinch" should "make the pokemon skip its turn" in:
+    val pokemon: BattlePokemon = PokemonTestUtils.simplePokemon4.copy(
+      volatileStatus = Set(assignedFlinch)
+    )
+
+    pokemon.volatileStatus.head.status.executeEffect(
+      pokemon,
+      initialTurn,
+      initialTurn
+    ).skipsCurrentTurn shouldBe true
