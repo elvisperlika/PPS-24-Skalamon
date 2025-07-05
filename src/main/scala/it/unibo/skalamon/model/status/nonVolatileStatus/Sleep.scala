ab2ef9ba87b6b450495e2664e5c87c049eaeb545
Norbert Gabos <dont.vip@gmail.com>
2025-07-05 16:46:42 +0200

    implement Sleep

---
+++ src/main/scala/it/unibo/skalamon/model/status/nonVolatileStatus/Sleep.scala
@@ -0,0 +1,24 @@
+package it.unibo.skalamon.model.status.nonVolatileStatus
+
+import it.unibo.skalamon.model.pokemon.BattlePokemon
+import it.unibo.skalamon.model.status.{AssignedStatus, NonVolatileStatus}
+
+/** Pokémon cannot act for 3 turns.
+  * @param turnsLeft
+  *   The number of turns left for the sleep status to expire. Defaults to
+  *   Sleep.SleepTurns.
+  */
+case class Sleep(turnsLeft: Int = Sleep.DefaultTurns) extends NonVolatileStatus:
+
+  override def executeEffect(pokemon: BattlePokemon): BattlePokemon =
+    if turnsLeft > 1 then
+      pokemon.copy(
+        skipsCurrentTurn = true,
+        nonVolatileStatus =
+          pokemon.nonVolatileStatus.map(_.copy(status = Sleep(turnsLeft - 1)))
+      )
+    else
+      pokemon.copy(nonVolatileStatus = None, skipsCurrentTurn = false)
+
+object Sleep:
+  val DefaultTurns: Int = 3
