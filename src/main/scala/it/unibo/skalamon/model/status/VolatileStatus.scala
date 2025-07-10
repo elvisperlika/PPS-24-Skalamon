ce77b8f9a23c69af844649bb128e64b1b9c2315c
Norbert Gabos <dont.vip@gmail.com>
2025-07-10 16:19:56 +0200

    add doc

--- src/main/scala/it/unibo/skalamon/model/status/VolatileStatus.scala
+++ src/main/scala/it/unibo/skalamon/model/status/VolatileStatus.scala
@@ -2,7 +2,19 @@ package it.unibo.skalamon.model.status
 
 import it.unibo.skalamon.model.pokemon.BattlePokemon
 
+/** Represents the Volatile status of a Pokémon.
+  */
 trait VolatileStatus extends Status:
+  /** Executes the effect of the volatile status on the given Pokémon.
+    * @param pokemon
+    *   The Pokémon to apply the status effect to.
+    * @param currentTurn
+    *   The current turn in the battle.
+    * @param assignedTurn
+    *   The turn when this status was assigned.
+    * @return
+    *   A copy of the Pokémon with the status effect applied.
+    */
   def executeEffect(
       pokemon: BattlePokemon,
       currentTurn: Int,
